4b59405463cc1b854f02c3b55a34c1da013501a7
karadagserhat <71098921+karadagserhat@users.noreply.github.com>
2020-10-25 09:35:28 +0300

    Add files via upload

---
+++ Dikdortgen.java
@@ -0,0 +1,15 @@
+package JFramee;
+
+
+
+public class Dikdortgen {
+
+	public static void main(String[] args) {
+		BizimPencere pencere = new BizimPencere();
+		pencere.setSize(640,480);
+		pencere.setVisible(true);
+		
+
+	}
+
+}
