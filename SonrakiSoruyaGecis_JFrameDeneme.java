dc5d6d866eecd7414f01b60ba9303255e2f31cbc
karadagserhat <71098921+karadagserhat@users.noreply.github.com>
2020-10-27 15:36:49 +0300

    Create SonrakiSoruyaGecis_JFrameDeneme.java

---
+++ SonrakiSoruyaGecis_JFrameDeneme.java
@@ -0,0 +1,20 @@
+package JFramee;
+
+import javax.swing.JFrame;
+
+public class JFrameDeneme {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		//SoruDeneme sd= new SoruDeneme();
+		
+		
+		//frame.add(sd);
+		frame.add(new SoruDeneme());
+		frame.setVisible(true);
+		frame.setBounds(100,100,200,150);
+		
+
+	}
+
+}
