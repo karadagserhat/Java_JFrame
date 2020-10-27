bd51bdc869d33f9de82b84463b1bbde922f45929
karadagserhat <71098921+karadagserhat@users.noreply.github.com>
2020-10-27 17:28:40 +0300

    Create ObjeYazmakVeOkumak_Kisi.java

---
+++ ObjeYazmakVeOkumak_Kisi.java
@@ -0,0 +1,19 @@
+package JFramee;
+
+import java.io.Serializable;
+
+public class Kisi implements Serializable {
+						   
+	String isim, soyad;
+	int yas; 
+
+	public Kisi() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
