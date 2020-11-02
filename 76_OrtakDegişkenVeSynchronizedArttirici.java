9c6fbd4427b3ff2fdaf21998fe711bd6d3fe30f4
karadagserhat <71098921+karadagserhat@users.noreply.github.com>
2020-11-02 09:52:17 +0300

    Create 76_OrtakDegişkenVeSynchronizedArttirici.java

---
+++ 76_OrtakDegişkenVeSynchronizedArttirici.java
@@ -0,0 +1,33 @@
+package Multii;
+
+public class Arttirici implements Runnable {
+	
+	MevcutSayi sayiReferansi;
+	int sure = 0;
+
+	public Arttirici(MevcutSayi gelenSayi) {
+		sayiReferansi = gelenSayi;
+	}
+
+	@Override
+	public void run() {
+		while(sure < 100)
+		{
+			synchronized (sayiReferansi) {
+				sayiReferansi.sayiDegeri++;
+				System.out.println("Arttırdı, sayı: " + sayiReferansi.sayiDegeri);
+			}
+			
+			
+			sure++;
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+}
